de9db2d986e4153e12b129dfc354d1282fc3afc8
AhmedAshraf <ahmed.ashraf@cloudx9.io>
2021-07-04 16:10:47 +0200

    pm2 deploy

--- index.ts
+++ index.ts
@@ -5,44 +5,54 @@ import moment from "moment";
 import { D_000_009, D_014_023, D_055_056 } from "./dialogues";
 import { D_023_071 } from "./dialogues/D_023_071";
 import "./helpers/i18n/i18n";
+import { MongoDbStorage } from "botbuilder-storage-mongodb";
+import { MongoClient } from "mongodb";
 
-let storage = undefined;
+// if (process.env.MONGO_URI) {
+console.log("MONGO_URI", process.env.MONGO_URI);
 
-if (process.env.MONGO_URI) {
-  console.log("MONGO_URI", process.env.MONGO_URI);
-  const { MongoDbStorage } = require("botbuilder-storage-mongodb");
-  storage = new MongoDbStorage({
-    url: process.env.MONGO_URI,
+export const botCtrl = (async () => {
+  const mongoClient = new MongoClient(process.env.MONGO_URI || "", {
+    useUnifiedTopology: true,
   });
-}
-const adapter = new WebAdapter({});
-//
-export const botCtrl = new Botkit({
-  webhook_uri: "/api/messages",
-  adapter: adapter,
-  storage: storage,
-});
+  mongoClient.connect();
 
-botCtrl; // botCtrl.publicFolder("/", path.join(__dirname, "..", "public"));
+  // Grab a collection handle off the connected client
+  const collection = MongoDbStorage.getCollection(mongoClient);
 
-const d_000_009 = D_000_009(botCtrl);
-D_014_023(botCtrl, d_000_009);
-D_055_056(botCtrl);
-D_023_071(botCtrl);
+  const storage = new MongoDbStorage(collection);
+  // }
 
-botCtrl.hears(
-  ["hello", "bot_start_action"],
-  "message",
-  async (bot, message) => {
-    I18n.locale = "ar";
-    moment.locale("ar");
-    await bot.cancelAllDialogs();
-    console.log(process.env.MONGO_URI);
+  const adapter = new WebAdapter({});
+  //
+  const botCtrl = new Botkit({
+    webhook_uri: "/api/messages",
+    adapter: adapter,
+    storage: storage,
+  });
 
-    return bot.beginDialog("d_000_009");
-  }
-);
+  botCtrl; // botCtrl.publicFolder("/", path.join(__dirname, "..", "public"));
 
-botCtrl.interrupts("NO_ZAKAT", "message", (bot, message) => {
-  return bot.beginDialog("d_055_056");
-});
+  const d_000_009 = D_000_009(botCtrl);
+  D_014_023(botCtrl, d_000_009);
+  D_055_056(botCtrl);
+  D_023_071(botCtrl);
+
+  botCtrl.hears(
+    ["hello", "bot_start_action"],
+    "message",
+    async (bot, message) => {
+      I18n.locale = "ar";
+      moment.locale("ar");
+      await bot.cancelAllDialogs();
+      console.log(process.env.MONGO_URI);
+
+      return bot.beginDialog("d_000_009");
+    }
+  );
+
+  botCtrl.interrupts("NO_ZAKAT", "message", (bot, message) => {
+    return bot.beginDialog("d_055_056");
+  });
+  return botCtrl;
+})();
