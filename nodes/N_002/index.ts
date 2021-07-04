c8aa72a40e6d9b791ddf2cfb16ccb3f4d4722ec0
AhmedAshraf <ahmed.ashraf@cloudx9.io>
2021-07-04 17:14:41 +0200

    pm2 deploy

--- nodes/N_002/index.ts
+++ nodes/N_002/index.ts
@@ -11,7 +11,7 @@ export default function NODE_002(convo: BotkitConversation): string {
         title: NODE_ID + ".opt1",
         payload: NODE_ID + ".choice0",
         onChoose: async (answer, convo, bot, msg) => {
-          convo.gotoThread("t_NODE_003");
+          convo.gotoThread("t_NODE_004");
         },
       },
       {
