9dcd7f35f77898aa950e112040143c3ce38c2d70
Cloudx9 <cloudx9@iMac.local>
2021-12-29 17:18:12 +0200

    fix

--- nodes/N_434/index.ts
+++ nodes/N_434/index.ts
@@ -20,7 +20,7 @@ export function NODE_434(convo: BotkitConversation): string {
         title: NODE_ID + ".opt2",
         payload: NODE_ID + ".choice1",
         onChoose: async (answer, convo, bot, msg) => {
-          convo.gotoThread("t_NODE_422");
+          convo.stop();
         },
       },
     ],
