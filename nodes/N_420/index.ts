b71154b45abf84fbd58bcf9f68b161af35bd6135
Your Name <you@example.com>
2022-11-18 18:08:33 +0200

    2khales integ

--- nodes/N_420/index.ts
+++ nodes/N_420/index.ts
@@ -11,7 +11,7 @@ export function NODE_420(convo: BotkitConversation): string {
         title: NODE_ID + ".opt1",
         payload: NODE_ID + ".choice0",
         onChoose: async (answer, convo, bot, msg) => {
-          convo.gotoThread("t_NODE_009");
+          convo.stop()
         },
       },
     ],
